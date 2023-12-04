5a4eb4897a7e9982304a2171a30f1d68d4d21a89
Lionel Brianto <kinobi@users.noreply.github.com>
2023-12-04 22:02:48 +0100

    feat: add solution for day 4 part two

--- src/Day4.php
+++ src/Day4.php
@@ -17,8 +17,8 @@ class Day4
     public function partOne(): int
     {
         $total = 0;
-
         $reader = $this->readInput($this->inputFilename);
+
         foreach ($reader as $card) {
             $re = '/Card\s+(\d+): ([(\d+)\s+]+)\| ([(\d+)\s+]+)/';
 
@@ -30,7 +30,7 @@ class Day4
             $wins = array_intersect($winningNumbers, $playedNumbers);
             $winCount = count($wins);
 
-            if($winCount <= 2) {
+            if ($winCount <= 2) {
                 $total += $winCount;
                 continue;
             }
@@ -39,5 +39,39 @@ class Day4
         }
 
         return $total;
+    }
+
+    public function partTwo(): int
+    {
+        $scratchcards = [];
+        $reader = $this->readInput($this->inputFilename);
+
+        foreach ($reader as $card) {
+            $re = '/Card\s+(\d+): ([(\d+)\s+]+)\| ([(\d+)\s+]+)/';
+
+            preg_match($re, $card, $matches);
+            $gameId = (int)$matches[1];
+            $winningNumbers = preg_split('/\s+/', trim($matches['2']));
+            $playedNumbers = preg_split('/\s+/', trim($matches['3']));
+
+            $wins = array_intersect($winningNumbers, $playedNumbers);
+            $winCount = count($wins);
+
+            // Add current scratchcard
+            isset($scratchcards[$gameId]) ?: $scratchcards[$gameId] = 0;
+            $scratchcards[$gameId]++;
+
+            if ($winCount === 0) {
+                continue;
+            }
+
+            // Add won scratchcard copies
+            for ($i = 1; $i <= $winCount; $i++) {
+                isset($scratchcards[$gameId + $i]) ?: $scratchcards[$gameId + $i] = 0;
+                $scratchcards[$gameId + $i] += $scratchcards[$gameId];
+            }
+        }
+
+        return array_sum($scratchcards);
     }
 }
--- tests/Unit/Day4Test.php
+++ tests/Unit/Day4Test.php
@@ -13,3 +13,15 @@ it('can give the total of winning point', function (string $filename, int $expec
         'Example' => [storage('04_test1.txt'), 13],
         'Solution' => [storage('04.txt'), 22674],
     ]);
+
+it('can give the total of won scratchcard', function (string $filename, int $expected) {
+    $day4 = new Day4($filename);
+
+    $total = $day4->partTwo();
+
+    expect($total)->toBe($expected);
+})
+    ->with([
+        'Example' => [storage('04_test1.txt'), 30],
+        'Solution' => [storage('04.txt'), 5747443],
+    ]);
